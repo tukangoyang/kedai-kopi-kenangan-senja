30feed3b4ff413eb00fa6285b754108d24de2920
tukangoyang <lanopaino@gmail.com>
2024-12-15 03:36:38 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// toggle clas active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// klik di luar sidbar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
